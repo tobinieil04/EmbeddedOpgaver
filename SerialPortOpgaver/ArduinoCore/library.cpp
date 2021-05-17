3b834f6638141f89295ea1aa087d59073566598d
Tobias Nielsen <tobi941b@elevcampus.dk>
2021-05-17 14:01:06 +0200

    Init

---
+++ SerialPortOpgaver/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 17-05-2021 13:24:08
+ * Author : tobiniel04
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ SerialPortOpgaver/BareMinimum/Sketch.cpp
@@ -0,0 +1,50 @@
+﻿/* 3. FormattedData.cpp
+	Print values in various formats to the serial port
+*/
+#include <Arduino.h>
+
+char chrValue  = 65;  // these are the starting values to print
+byte byteValue = 65;
+int intValue   = 65;
+float floatValue = 65.0;
+char c1 = 4;
+char c2 = 210;
+
+void setup()
+{
+	while(!Serial); // Wait until serial port's open on Leonardo and SAMD boards
+	Serial.begin(115200);
+}
+
+void loop()
+{
+	Serial.print("chrValue:   ");
+	Serial.print(chrValue); Serial.print(" ");
+	Serial.write(chrValue); Serial.print(" ");
+	Serial.print(chrValue, DEC);
+	Serial.println();
+
+	Serial.print("byteValue:  ");
+	Serial.print(byteValue); Serial.print(" ");
+	Serial.write(byteValue); Serial.print(" ");
+	Serial.print(byteValue, DEC);
+	Serial.println();
+
+	Serial.print("intValue:   ");
+	Serial.print(intValue); Serial.print(" ");
+	Serial.print(intValue, DEC); Serial.print(" ");
+	Serial.print(intValue, HEX); Serial.print(" ");
+	Serial.print(intValue, OCT); Serial.print(" ");
+	Serial.print(intValue, BIN);
+	Serial.println();
+
+	Serial.print("floatValue: ");
+	Serial.println(floatValue);
+	Serial.println();
+
+	delay(1000); // delay a second between numbers
+	chrValue++;  // to the next value
+	byteValue++;
+	intValue++;
+	floatValue += 1;
+}
