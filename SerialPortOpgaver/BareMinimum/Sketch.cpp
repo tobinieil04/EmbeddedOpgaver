0b94566ebab72d2df7f8016b3f34beea5a54a258
Tobias Nielsen <tobi941b@elevcampus.dk>
2021-05-17 15:33:18 +0200

    fuck

--- SerialPortOpgaver/BareMinimum/Sketch.cpp
+++ SerialPortOpgaver/BareMinimum/Sketch.cpp
@@ -3,48 +3,37 @@
 */
 #include <Arduino.h>
 
-char chrValue  = 65;  // these are the starting values to print
-byte byteValue = 65;
-int intValue   = 65;
-float floatValue = 65.0;
-char c1 = 4;
-char c2 = 210;
+int led = 11;
+int incommingNum = 0;
 
 void setup()
 {
 	while(!Serial); // Wait until serial port's open on Leonardo and SAMD boards
 	Serial.begin(115200);
+	pinMode(led, OUTPUT);
+	
 }
 
 void loop()
 {
-	Serial.print("chrValue:   ");
-	Serial.print(chrValue); Serial.print(" ");
-	Serial.write(chrValue); Serial.print(" ");
-	Serial.print(chrValue, DEC);
-	Serial.println();
+	
+	double del = (incommingNum - 48) * 0.1 * 1000;
+	digitalWrite(led, HIGH);
+	delay(del);
+	digitalWrite(led, LOW);
+	delay(del);
+	
+}
 
-	Serial.print("byteValue:  ");
-	Serial.print(byteValue); Serial.print(" ");
-	Serial.write(byteValue); Serial.print(" ");
-	Serial.print(byteValue, DEC);
-	Serial.println();
-
-	Serial.print("intValue:   ");
-	Serial.print(intValue); Serial.print(" ");
-	Serial.print(intValue, DEC); Serial.print(" ");
-	Serial.print(intValue, HEX); Serial.print(" ");
-	Serial.print(intValue, OCT); Serial.print(" ");
-	Serial.print(intValue, BIN);
-	Serial.println();
-
-	Serial.print("floatValue: ");
-	Serial.println(floatValue);
-	Serial.println();
-
-	delay(1000); // delay a second between numbers
-	chrValue++;  // to the next value
-	byteValue++;
-	intValue++;
-	floatValue += 1;
+void serialEvent() 
+{
+	incommingNum = Serial.read();
+	if(incommingNum > 9 || incommingNum < 0)
+	{
+		exit(0);
+	}
+	else
+	{
+		
+	}
 }
